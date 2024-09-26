0a0d1064a23eafb6469c7e6ba7083101752985d9
TarikVu <u0759288@utah.edu>
2024-09-26 11:25:12 -0600

    updated controlbar ui

--- src/components/controlbar.js
+++ src/components/controlbar.js
@@ -22,45 +22,48 @@ const ControlBar = ({
 
   return (
     <div className="control-bar">
-
       <div className='title-description'>
         <h1 className='title'>Title</h1>
         <p className='description'>Description Lorem itsum oppous naDescription Lorem itsum oppous narutoDescription Lorem itsum oppous narutoDescription Lorem itsum oppous narutoDescription Lorem itsum oppous narutoDescription Lorem itsum oppous narutoDescription Lorem itsum oppous narutoDescription Lorem itsum oppous narutoDescription Lorem itsum oppous narutoDescription Lorem itsum oppous narutoDescription Lorem itsum oppous narutoDescription Lorem itsum oppoDescription Lorem itsum oppous narutoDescription Lorem itsum oppous narutoDescription Lorem itsum oppous narutoDescription Lorem itsum oppous narutoDescription Lorem itsum oppous narutoDescription Lorem itsum oppous narutoDescription Lorem itsum oppous narutoDescription Lorem itsum oppous narutoDescription Lorem itsum oppous narutoDescription Lorem itsum oppous narutous narutoDescription Lorem itsum oppous narutorutoDescription Lorem itsum oppous narutoDescription Lorem itsum oppous narutoDescription Lorem itsum oppous narutoDescription Lorem itsum oppous narutoDescription Lorem itsum oppous narutoDescription Lorem itsum oppous naruto</p>
       </div>
 
       <div className='controls'>
-        <BoardForm
-          isVisible={isBoardFormVisible}
-          onClose={() => setIsBoardFormVisible(false)}
-          addBoard={addBoard}
-        />
+        <div className='controls-select'>
+          <p>Select a board</p>
+          <select onChange={handleBoardChange} value={selectedBoard._id || ''}>
+            <option value=""></option>
+            {allBoards.map(board => (
+              <option key={board._id} value={board._id}>
+                {board.title}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className='controls-buttons'>
+          <BoardForm
+            isVisible={isBoardFormVisible}
+            onClose={() => setIsBoardFormVisible(false)}
+            addBoard={addBoard}
+          />
 
-        <ColumnForm
-          isVisible={isColumnFormVisible}
-          onClose={() => setIsColumnFormVisible(false)}
-          addColumn={addColumn}
-        />
+          <ColumnForm
+            isVisible={isColumnFormVisible}
+            onClose={() => setIsColumnFormVisible(false)}
+            addColumn={addColumn}
+          />
+          <button onClick={() => delBoard({ boardId: selectedBoard._id })}>
+            Delete Board
+          </button>
+          <button onClick={() => setIsBoardFormVisible(true)}>
+            New Board
+          </button>
 
-        <button onClick={() => setIsBoardFormVisible(true)}>
-          Add New Board
-        </button>
+          <button onClick={() => setIsColumnFormVisible(true)}>
+            New Column
+          </button>
 
-        <button onClick={() => setIsColumnFormVisible(true)}>
-          Add New Column
-        </button>
 
-        <button onClick={() => delBoard({ boardId: selectedBoard._id })}>
-          Delete Board
-        </button>
-
-        <select onChange={handleBoardChange} value={selectedBoard._id || ''}>
-          <option value="">Select a Board</option>
-          {allBoards.map(board => (
-            <option key={board._id} value={board._id}>
-              {board.title}
-            </option>
-          ))}
-        </select>
+        </div>
 
       </div>
     </div >
