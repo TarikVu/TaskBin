6953c6c4c979c363d877b5e5dfbbb2abeded804f
TarikVu <tarikvu2@gmail.com>
2024-11-05 12:59:32 -0700

    serving

--- server/server.js
+++ server/server.js
@@ -375,14 +375,16 @@ app.patch('/move', async (req, res) => {
 
 
 const path = require('path');
-app.use(express.static(path.join(__dirname, 'client/build'))); // Adjust the path accordingly
+
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
+
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
